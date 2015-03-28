9b77184a0c1702f597accc9bb6edb99c5b9a5827
myn <myn@dktp-win.telecom>
2015-03-28 13:38:46 +1300

    Added width and height properties to Basic player, made the player a different color for easier debugging hitbox, cleaned code, disabled unused variables

--- GameScreen/BasicPlayer.java
+++ GameScreen/BasicPlayer.java
@@ -12,91 +12,123 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-
 public class BasicPlayer {
+    // co-ordinates of player
+    int x;
+    int y;
     
-        int setRateOfFire = 1000;
-        BulletHandler bullets = new BulletHandler(-1 , setRateOfFire);
-	int x = 400;
-	int xa = 0;
-        int y = 460;
-        boolean move_left,move_right;
-        Color color = Color.DARK_GRAY;
-        int score;
-        ImageIcon image;
-        int health = 30;
-        
-        
-        // Player constructor
-	public BasicPlayer(){
-            
-	}
+    // player size
+    int height;
+    int width;
+    
+    int fireRate;
+    boolean move_left, move_right;
+    Color color;
+    int score;
+    int health;
+    BulletHandler bullets = new BulletHandler(-1, fireRate);
+    
+    // Unused
+    //int xa;
+    //ImageIcon image;
 
-        // Moves Player aswell as bullets
-	public void move(JPanel win) {
-            
-            if(move_left && (x - 1 > 0 && x - 1 < win.getWidth()-60)){
-                x += -1;
-            }
-            if(move_right && (x + 1 > 0 && x + 1 < win.getWidth()-60)){
-                x += 1;
-            }
-            bullets.move();
-            bullets.kill();
-       
-	}
+    // Player constructor
+    public BasicPlayer() {
+        // call default constructor with default width and height
+        this(30,2);
+    }
 
-        // Paints player and bullets, determins look of player
-	public void paint(Graphics2D g) {
-                g.setColor(color);
-		g.fillRect(x, y, 60, 10);
-                bullets.paint(g);
-                g.setColor(Color.WHITE);
-                g.drawString("Score:", 20, 20);
-                g.drawString(String.valueOf(score), 100, 20);
-                g.drawString("Health:", 200, 20);
-                g.drawString(String.valueOf(health), 280, 20);
-	}
+    public BasicPlayer(int width, int height) {
+        this.width = width;
+        this.height = height;
+        
+        // initialise other values
+        x = 400;
+        y = 460;
+        fireRate = 1000;
+        color = Color.GREEN;
+        health = 30;
+    }
+    
+    
 
-        // Player mechanics
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-                        move_left = false;
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-                        move_right = false;
-	}
-        
-        // Player mechanics
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			//xa = -1;
-                        move_left = true;
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-			//xa = 1;
-                        move_right = true;
-                if (e.getKeyCode() == KeyEvent.VK_SPACE)
-			bullets.spawnMissile(x + 30, y);
-	}
-        
-        // Returns bullets array
-        public ArrayList getAmo(){
-            return bullets.getbullets();
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+    // Moves Player as well as bullets
+    public void move(JPanel win) {
+        if (move_left && (x - 1 > 0 && x - 1 < win.getWidth() - 60)) {
+            x += -1;
         }
-        
-        // Removes bullet and increases player score
-        public void hit(int i){
-            bullets.remove(i);
-            score = score + 100;
-            
+        if (move_right && (x + 1 > 0 && x + 1 < win.getWidth() - 60)) {
+            x += 1;
         }
-        
-        public int gety(){
-            return y;
+        bullets.move();
+        bullets.kill();
+
+    }
+
+    // Paints player and bullets, determins look of player
+    public void paint(Graphics2D g) {
+        g.setColor(color);
+        g.fillRect(x, y, 60, 10);
+        bullets.paint(g);
+        g.setColor(Color.WHITE);
+        g.drawString("Score:", 20, 20);
+        g.drawString(String.valueOf(score), 100, 20);
+        g.drawString("Health:", 200, 20);
+        g.drawString(String.valueOf(health), 280, 20);
+    }
+
+    // Player mechanics
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            move_left = false;
         }
-        
-        public int getx(){
-            return x;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            move_right = false;
         }
+    }
+
+    // Player mechanics
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) //xa = -1;
+        {
+            move_left = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) //xa = 1;
+        {
+            move_right = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            bullets.spawnMissile(x + 30, y);
+        }
+    }
+
+    // Returns bullets array
+    public ArrayList getAmo() {
+        return bullets.getbullets();
+    }
+
+    // Removes bullet and increases player score
+    public void hit(int i) {
+        bullets.remove(i);
+        score = score + 100;
+
+    }
+
+    public int gety() {
+        return y;
+    }
+
+    public int getx() {
+        return x;
+    }
 
     int health() {
         return health;
@@ -104,13 +136,11 @@ public class BasicPlayer {
 
     void playerHit() {
         health -= 1;
-        reset();
+        locationRespawn();
     }
-    
-    public void reset(){
+
+    public void locationRespawn() {
         x = 400;
-	xa = 0;
         y = 460;
     }
-        
 }
--- GameScreen/GamePanel.java
+++ GameScreen/GamePanel.java
@@ -1,7 +1,4 @@
-
 package GameScreen;
-
-
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,9 +11,8 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
 public class GamePanel extends JPanel {
-    
+
     JPanel temp_this = this;
     // Game Screen Entities
     EnemyHandler invaders = new EnemyHandler();
@@ -26,129 +22,134 @@ public class GamePanel extends JPanel {
     int paint_updateInterval = 1;
     int player_updateInterval = 5;
     int enemy_updateInterval = 10;
-    
+
     // gameScreen Constructor
     public GamePanel() {
-                
-                // ActionListener for time, what happens when timer executes
-                this.paint_timer = new Timer(paint_updateInterval,(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    repaint();
-                }
-                }));
-                
-                this.player_timer = new Timer(player_updateInterval,(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    shooter.move(temp_this);
-                }
-                }));
-                
-                this.enemy_timer = new Timer(enemy_updateInterval,(new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    invaders.moveArmy(temp_this);
-                    checkCollision();
-                }
-                }));
-                
-                
-                // Gets User Input
-		addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-			}
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-                            shooter.keyReleased(e);
-			}
+        // ActionListener for time, what happens when timer executes
+        this.paint_timer = new Timer(paint_updateInterval, (new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+            }
+        }));
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-                            if (!enemy_timer.isRunning()){
-                                if (e.getKeyCode() == KeyEvent.VK_SPACE){
-                                    enemy_timer.start();
-                                    player_timer.start();
-                                }
-                            }
-                            if(e.getKeyCode() == KeyEvent.VK_P){
-                                 enemy_timer.stop();
-                                 player_timer.stop();
-                             }
-                            if(e.getKeyCode() == KeyEvent.VK_R){
-                                restart();
-                             }
-                            shooter.keyPressed(e);
-			}
-		});
-                this.setFocusable(true);
-                this.requestFocus();
-                paint_timer.start();
-	}
-    
+        this.player_timer = new Timer(player_updateInterval, (new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                shooter.move(temp_this);
+            }
+        }));
+
+        this.enemy_timer = new Timer(enemy_updateInterval, (new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                invaders.moveArmy(temp_this);
+                checkCollision();
+            }
+        }));
+
+        // Gets User Input
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                shooter.keyReleased(e);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!enemy_timer.isRunning()) {
+                    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                        enemy_timer.start();
+                        player_timer.start();
+                    }
+                }
+                if (e.getKeyCode() == KeyEvent.VK_P) {
+                    enemy_timer.stop();
+                    player_timer.stop();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_R) {
+                    restart();
+                }
+                shooter.keyPressed(e);
+            }
+        });
+        this.setFocusable(true);
+        this.requestFocus();
+        paint_timer.start();
+    }
+
     @Override
     public void paint(Graphics win) {
         // Clears window
-	super.paint(win);
-        Graphics2D window = (Graphics2D)win;
+        super.paint(win);
+        Graphics2D window = (Graphics2D) win;
         window.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         invaders.paint(window);
         shooter.paint(window);
-	}
-    
+    }
+
     // Moves Entities and passes panel information
-    public void move(){
+    public void move() {
         invaders.moveArmy(this);
         shooter.move(this);
     }
-    
+
     // Needs to be re-written, messy
-    public void checkCollision(){
+    public void checkCollision() {
         ArrayList<BasicEnemy> badies = invaders.getArmy();
         ArrayList<Bullet> pewpew = shooter.getAmo();
         int temp, temp2;
-        for(temp = 0;temp!=badies.size();++temp){
-            if (temp >= badies.size()){return;}
-            for(temp2 = 0; temp2 !=pewpew.size();++temp2){
-                if (temp2 >= pewpew.size()){return;}
-                    if (((badies.get(temp).getx()-30) <= pewpew.get(temp2).getx() )&((badies.get(temp).getx() + 30 )>= (pewpew.get(temp2).getx() + 2))){
-                      if (((badies.get(temp).gety()-30) <= pewpew.get(temp2).gety() )&((badies.get(temp).gety() + 30) >= (pewpew.get(temp2).gety() + 2))){ 
-                          invaders.hit(temp);
-                          shooter.hit(temp2);
-                          temp -= 1;
-                          temp2 -= 1;
-                          if (temp == -1 | temp2 == -1){return;}
+        for (temp = 0; temp != badies.size(); ++temp) {
+            if (temp >= badies.size()) {
+                return;
             }
+            for (temp2 = 0; temp2 != pewpew.size(); ++temp2) {
+                if (temp2 >= pewpew.size()) {
+                    return;
+                }
+                if (((badies.get(temp).getx() - 30) <= pewpew.get(temp2).getx()) & ((badies.get(temp).getx() + 30) >= (pewpew.get(temp2).getx() + 2))) {
+                    if (((badies.get(temp).gety() - 30) <= pewpew.get(temp2).gety()) & ((badies.get(temp).gety() + 30) >= (pewpew.get(temp2).gety() + 2))) {
+                        invaders.hit(temp);
+                        shooter.hit(temp2);
+                        temp -= 1;
+                        temp2 -= 1;
+                        if (temp == -1 | temp2 == -1) {
+                            return;
+                        }
+                    }
+                }
+
             }
-            
-        }
         }
         pewpew = invaders.getAmmo();
         int x = shooter.getx() + 30;
         int y = shooter.gety() + 5;
-        for(int k = 0;k != pewpew.size();k++ ){
-            if (((x-30) <= pewpew.get(k).getx() )&((x + 30 )>= (pewpew.get(k).getx() + 2))){
-                      if (((y) <= pewpew.get(k).gety() )&((y + 10) >= (pewpew.get(k).gety() + 2))){ 
-                          shooter.playerHit();
-                          pewpew.remove(k);
-                      }
+        for (int k = 0; k != pewpew.size(); k++) {
+            if (((x - 30) <= pewpew.get(k).getx()) & ((x + shooter.getWidth()) >= (pewpew.get(k).getx() + shooter.getHeight()))) {
+                if (((y) <= pewpew.get(k).gety()) & ((y + 10) >= (pewpew.get(k).gety() + 2))) {
+                    shooter.playerHit();
+                    pewpew.remove(k);
+                }
             }
         }
-        }
-    
-    public void restart(){
+    }
+
+    public void restart() {
         invaders = new EnemyHandler();
         enemy_timer.stop();
         player_timer.stop();
-        shooter.reset();
+        shooter.locationRespawn();
     }
-             
+
     // Checks if any enemys are left
-    public boolean winner(){
-       return invaders.getArmy().isEmpty();
-   }
+    public boolean winner() {
+        return invaders.getArmy().isEmpty();
+    }
 
     public boolean looser() {
         return (shooter.health() == 0);
